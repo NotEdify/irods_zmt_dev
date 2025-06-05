7eadf67f1f05876d84b34e15b09da8e5dc59a298
ramsey <NotEdify@proton.me>
2025-06-04 23:58:28 -0400

    removed axios from group controller

--- src/controllers/GroupController.js
+++ src/controllers/GroupController.js
@@ -1,74 +1,81 @@
-import axios from 'axios';
-
 export const AddGroupController = (name, httpApiLocation) => {
-    const params = new URLSearchParams({
-        op: "create_group",
-        name: name,
-    });
+  const url = `${httpApiLocation}/users-groups`;
+  const params = new URLSearchParams({
+    op: "create_group",
+    name: name,
+  });
 
-    return axios.post(
-        `${httpApiLocation}/users-groups`,
-        params,
-        {
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem('zmt-token')}`,
-            }
-        } 
-    );
+  return fetch(url, {
+    method: "POST",
+    body: params.toString(),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+      Authorization: `Bearer ${localStorage.getItem("zmt-token")}`,
+    },
+  });
 };
 
 export const RemoveGroupController = async (name, httpApiLocation) => {
-    const params = new URLSearchParams({
-        op: "remove_group",
-        name: name,
-    });
+  const url = `${httpApiLocation}/users-groups`;
+  const params = new URLSearchParams({
+    op: "remove_group",
+    name: name,
+  });
 
-    return axios.post(
-        `${httpApiLocation}/users-groups`,
-        params,
-        {
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem('zmt-token')}`,
-            }
-        } 
-    );
+  return fetch(url, {
+    method: "POST",
+    body: params.toString(),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+      Authorization: `Bearer ${localStorage.getItem("zmt-token")}`,
+    },
+  });
 };
 
-export const AddUserToGroupController = (user, zone, group, httpApiLocation) => {
-    const params = new URLSearchParams({
-        op: 'add_to_group',
-        user: user,
-        zone: zone,
-        group: group,
-    });
+export const AddUserToGroupController = (
+  user,
+  zone,
+  group,
+  httpApiLocation,
+) => {
+  const url = `${httpApiLocation}/users-groups`;
+  const params = new URLSearchParams({
+    op: "add_to_group",
+    user: user,
+    zone: zone,
+    group: group,
+  });
 
-    return axios.post(
-        `${httpApiLocation}/users-groups`,
-        params,
-        {
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem('zmt-token')}`,
-            }
-        } 
-    );
+  return fetch(url, {
+    method: "POST",
+    body: params.toString(),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+      Authorization: `Bearer ${localStorage.getItem("zmt-token")}`,
+    },
+  });
 };
 
-export const RemoveUserFromGroupController = (user, zone, group, httpApiLocation) => {
-    const params = new URLSearchParams({
-        op: 'remove_from_group',
-        user: user,
-        zone: zone,
-        group: group,
-    });
+export const RemoveUserFromGroupController = (
+  user,
+  zone,
+  group,
+  httpApiLocation,
+) => {
+  const url = `${httpApiLocation}/users-groups`;
+  const params = new URLSearchParams({
+    op: "remove_from_group",
+    user: user,
+    zone: zone,
+    group: group,
+  });
 
-    return axios.post(
-        `${httpApiLocation}/users-groups`,
-        params,
-        {
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem('zmt-token')}`,
-            }
-        } 
-    );
+  return fetch(url, {
+    method: "POST",
+    body: params.toString(),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+      Authorization: `Bearer ${localStorage.getItem("zmt-token")}`,
+    },
+  });
 };
-
